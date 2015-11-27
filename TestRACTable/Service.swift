23c888d83b9f8450e7d4c0adc8758ccbf0842a74
Pakorn Techaveerapong <pakorn@kitchenup.com>
2015-11-27 13:06:25 +0700

    first commit: Model and VM

---
+++ TestRACTable/Service.swift
@@ -0,0 +1,36 @@
+//
+//  Service.swift
+//  TestRACTable
+//
+//  Created by Pakorn Techaveerapong on 11/27/2558 BE.
+//  Copyright © 2558 Pakorn Techaveerapong. All rights reserved.
+//
+
+import UIKit
+import ReactiveCocoa
+
+class Service: NSObject {
+
+    func getList() -> SignalProducer<[Model]?, NSError> {
+        return SignalProducer {
+            observer, disposable in
+            let delay = UInt64(2.0) * NSEC_PER_SEC
+            let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
+            dispatch_after(delayTime, dispatch_get_main_queue(), {
+                let models = self.buildRandomArrayOfModel()
+                observer.sendNext(models)
+                observer.sendCompleted()
+            })
+        }
+    }
+    
+    func buildRandomArrayOfModel() -> Array<Model> {
+        var array: Array<Model> = []
+        for i in 1...30 {
+            array.append(Model("Name \(i)"))
+        }
+        
+        return array
+    }
+    
+}
---
+++ ViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ViewModel.swift
+//  TestRACTable
+//
+//  Created by Pakorn Techaveerapong on 11/27/2558 BE.
+//  Copyright © 2558 Pakorn Techaveerapong. All rights reserved.
+//
+
+import UIKit
+
+class ViewModel: NSObject {
+
+}
