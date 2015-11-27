7d6720e2dbbde1f78db85b0e434a0cb4734269a7
Pakorn Techaveerapong <pakorn@kitchenup.com>
2015-11-27 16:44:44 +0700

    finish mvc

--- TestRACTable/Service.swift
+++ TestRACTable/Service.swift
@@ -7,21 +7,25 @@
 //
 
 import UIKit
-import ReactiveCocoa
 
 class Service: NSObject {
 
-    func getList() -> SignalProducer<[Model]?, NSError> {
-        return SignalProducer {
-            observer, disposable in
-            let delay = UInt64(2.0) * NSEC_PER_SEC
-            let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
-            dispatch_after(delayTime, dispatch_get_main_queue(), {
-                let models = self.buildRandomArrayOfModel()
-                observer.sendNext(models)
-                observer.sendCompleted()
-            })
-        }
+//    func getList() -> SignalProducer<[Model]?, NSError> {
+//        return SignalProducer {
+//            observer, disposable in
+//            let delay = UInt64(2.0) * NSEC_PER_SEC
+//            let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
+//            dispatch_after(delayTime, dispatch_get_main_queue(), {
+//                let models = self.buildRandomArrayOfModel()
+//                observer.sendNext(models)
+//                observer.sendCompleted()
+//            })
+//        }
+//    }
+    
+    func getList(completeBlock: ([Model]? -> Void)) {
+        let data = buildRandomArrayOfModel()
+        completeBlock(data)
     }
     
     func buildRandomArrayOfModel() -> Array<Model> {
--- TestRACTable/ViewController.swift
+++ TestRACTable/ViewController.swift
@@ -8,15 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
     
-    let viewModel = ViewModel()
+    let service = Service()
+    var data: [Model]?
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        viewModel.getData()
+        service.getList {
+            array in
+            self.data = array
+            self.tableView.reloadData()
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,6 +30,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.data?.count ?? 0
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell")
+        cell?.textLabel?.text = self.data![indexPath.row].name
+        return cell!
+    }
 
 }
 
