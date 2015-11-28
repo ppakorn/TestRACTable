d5d06c4da2c3435937ec9c39296beae760528874
Pakorn Techaveerapong <pakorn@kitchenup.com>
2015-11-28 10:34:48 +0700

    add reloaddata and reloadtable button

--- TestRACTable/ViewController.swift
+++ TestRACTable/ViewController.swift
@@ -13,12 +13,15 @@ class ViewController: UIViewController {
     private let viewModel = ViewModel()
     private var bindingHelper: TableBindingHelper?
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var reloadDataButton: UIButton!
+    @IBOutlet weak var reloadTableButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         bindViewModel()
+        bindButton()
         viewModel.getData()
     }
 
@@ -29,6 +32,18 @@ class ViewController: UIViewController {
     
     func bindViewModel() {
         self.bindingHelper = TableBindingHelper(tableView: self.tableView, viewModel: self.viewModel)
+    }
+    
+    func bindButton() {
+        reloadDataButton.rac_signalForControlEvents(.TouchUpInside).subscribeNext {
+            _ in
+            self.viewModel.getData()
+        }
+        
+        reloadTableButton.rac_signalForControlEvents(.TouchUpInside).subscribeNext {
+            _ in
+            self.tableView.reloadData()
+        }
     }
 
 }
